6ecbdb7862794748808ffaf3c018aa4b007453dc
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-08-19 07:17:41 -0500

    Initial commit, added in base files, filled out index, sourced in angular and client

---
+++ public/vendors/client.js
@@ -0,0 +1,7 @@
+var app = angular.module('EmployeeApp', []);
+
+app.controller('EmployeeController', ['$http', function($http){
+    console.log('Employee Controller is ready!');
+    var self = this;
+    
+}]);
